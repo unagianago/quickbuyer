34aa4654ce452d3e99c86afb73e2e1cd3c473ab5
unagianago <fumiyakm4580@gmail.com>
2024-06-03 14:03:43 +0900

    rubocopによるコード修正

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -2,16 +2,14 @@ class DealersController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
-    @categories = Category.all
     @buyers = Buyer.all
   end
-  
+
   private
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    return if user_signed_in?
+
+    redirect_to action: :index
   end
-  
 end
