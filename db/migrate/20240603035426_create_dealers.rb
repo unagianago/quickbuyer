b872df6396633a5ca89278a2bed58c1c701c8146
unagianago <fumiyakm4580@gmail.com>
2024-06-03 12:58:25 +0900

    モデルとコントローラーを再作成

---
+++ db/migrate/20240603035426_create_dealers.rb
@@ -0,0 +1,9 @@
+class CreateDealers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dealers do |t|
+      t.integer    :buyer_id, null: false
+      t.integer    :category_id, null: false
+      t.timestamps
+    end
+  end
+end
