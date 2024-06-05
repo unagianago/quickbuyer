501b7345de083759143b9f5964b8e196dbc4f498
unagianago <fumiyakm4580@gmail.com>
2024-06-05 09:48:09 +0900

    コントローラーの設定

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -1,11 +1,13 @@
 class DealersController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :search]
 
   def index
     @buyers = Buyer.all
   end
 
-
+  def search
+    @buyers = Buyer.where(name: params[:name],category_id: params[:category_id])
+  end
 
   private
 
