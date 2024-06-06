3da8e81e49b1823e5e1f06a412952ea797860384
unagianago <fumiyakm4580@gmail.com>
2024-06-06 10:36:19 +0900

    モデルとデータベースの作成

--- README.md
+++ README.md
@@ -17,14 +17,14 @@
 | Column             | Type       | Options                         |
 | ------------------ | ---------- | ------------------------------- |
 | user               | references | null: false, foreign_key: true  |
-| dealer             | references | null: false, foreign_key: true  |
+| buyer_id           |  integer   | null: false                     |
 
 ### Association
 
 - belongs_to :user
 - belongs_to :dealer
 
-## dealers テーブル
+## ActiveHash
 
 | Column             | Type       | Options                     |
 | ------------------ | ---------- | --------------------------- |
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_02_232736) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_06_012625) do
+  create_table "favorites", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "buyer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_02_232736) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "users"
 end
