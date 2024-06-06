db66e5a8e29806a30c3e8af50975a628bc7eaabe
unagianago <fumiyakm4580@gmail.com>
2024-06-06 12:04:52 +0900

    indexアクションを定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def index
+    @favorite_buyers = @user.favorite_buyers
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
 end
