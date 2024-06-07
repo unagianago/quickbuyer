e2bf075b1b4d17c86304334702f6d7b868eae214
unagianago <fumiyakm4580@gmail.com>
2024-06-07 14:15:37 +0900

    本番環境で動作させる設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,10 @@ class Favorite < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :buyer, class_name: 'Buyer'
+
+  validates :buyer_id, numericality: true, presence: true
+  validates :user_id, numericality: true, presence: true
+
+  validates :user_id, uniqueness: { scope: :buyer_id}
+
 end
