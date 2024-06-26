ed9e113ee0dabc16d84fc445fd5f4ccd8cff4f09
unagianago <fumiyakm4580@gmail.com>
2024-06-26 20:03:02 +0900

    買取カテゴリーと買取方法を更新

--- app/models/method.rb
+++ app/models/method.rb
@@ -3,8 +3,7 @@ class Method < ActiveHash::Base
     { id: 1, name: '持ち込み' },
     { id: 2, name: '郵送' },
     { id: 3, name: '出張' },
-    { id: 4, name: 'オンライン査定' },
-    { id: 5, name: 'リサイクルショップへの持ち込み' }
+    { id: 4, name: 'オンライン査定' }
   ]
 
   include ActiveHash::Associations
